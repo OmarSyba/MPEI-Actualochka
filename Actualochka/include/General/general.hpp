e900552552ecf39bcd58e69e773582d1d5b02a8a
OmarSyba <sybabubik@gmail.com>
2021-03-27 19:11:38 +0300

    v1.0.4

--- Actualochka/include/General/general.hpp
+++ Actualochka/include/General/general.hpp
@@ -12,7 +12,7 @@ static QFile logfile(logdir + "/Act.log");
 
 namespace act
 {
-    static QString CurrnetVersion = "1.0.3";
+    static QString CurrnetVersion = "1.0.4";
     static QString MpeiActuallity = "https://mpei-server.herokuapp.com/api/getActuality";
     static QString MpeiSchedule = "https://mpei-server.herokuapp.com/api/getSchedule";
     static QString MpeiVersion = "https://mpei-win.herokuapp.com/version";
